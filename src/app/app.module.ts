71c0275c57adf1217f86fbe19381c5e78cae68e5
nhutphi456 <vonhutphi>
2023-09-27 13:32:06 +0700

    add ngrx/effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, EffectsModule.forRoot([])],
   providers: [],
   bootstrap: [AppComponent],
 })
