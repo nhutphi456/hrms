2d5f142e65900ed8faf8f31f506acef5b6cb220e
nhutphi456 <vonhutphi>
2023-09-27 16:28:36 +0700

    add layout html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppLayoutComponent } from './layout/app-layout.component';
 
 const routes: Routes = [
   {path: 'login', loadChildren: () => import('./components/login/login.module').then(m => m.LoginModule)},
+  {
+    path: "",
+    component: AppLayoutComponent,
+  }
 ];
 
 @NgModule({
---
+++ src/app/layout/models/menu.model.ts
@@ -0,0 +1,38 @@
+export interface MenuItem {
+  name: string;
+  icon: string;
+  path: string;
+  submenu: {
+    title: string;
+    items: MenuElementItem[];
+  };
+}
+export class MenuElementItem {
+  name: string;
+
+  content: string;
+
+  path: string;
+
+  icon: string;
+
+  param?: object;
+
+  value?: number;
+
+  constructor(
+    name: string,
+    content: string,
+    path: string,
+    icon: string,
+    param?: object,
+    value?: number,
+  ) {
+    this.name = name;
+    this.content = content;
+    this.path = path;
+    this.icon = icon;
+    this.param = param;
+    this.value = value;
+  }
+}
