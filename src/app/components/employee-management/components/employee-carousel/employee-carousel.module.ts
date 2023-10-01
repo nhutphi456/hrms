2925ef7510bfad27d0b2b7482c69e41c2db5c249
nhutphi456 <vonhutphi>
2023-10-01 20:49:28 +0700

    featt(ui): style tab menu

--- src/app/components/employee-management/components/employee-carousel/employee-carousel.module.ts
+++ src/app/components/employee-management/components/employee-carousel/employee-carousel.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {CarouselModule} from 'primeng/carousel';
-import {AvatarModule} from 'primeng/avatar';
+import { CarouselModule } from 'primeng/carousel';
+import { AvatarModule } from 'primeng/avatar';
+import { TagModule } from 'primeng/tag';
 import { EmployeeCarouselComponent } from './employee-carousel.component';
 
 @NgModule({
   declarations: [EmployeeCarouselComponent],
-  imports: [
-    CommonModule, CarouselModule, AvatarModule
-  ],
-  exports: [EmployeeCarouselComponent]
+  imports: [CommonModule, CarouselModule, AvatarModule, TagModule],
+  exports: [EmployeeCarouselComponent],
 })
-export class EmployeeCarouselModule { }
+export class EmployeeCarouselModule {}
