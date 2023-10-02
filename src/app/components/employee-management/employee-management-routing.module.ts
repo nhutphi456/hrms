1b2956ea5dd78bb950aa37a281b4656552022336
nhutphi456 <vonhutphi>
2023-10-02 13:01:03 +0700

    feat(ui): employee detail

--- src/app/components/employee-management/employee-management-routing.module.ts
+++ src/app/components/employee-management/employee-management-routing.module.ts
@@ -1,15 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeDetailComponent } from './components/employee-detail/employee-detail.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
 import { EmployeeManagementComponent } from './employee-management.component';
 
-const routes: Routes = [{
-  path: '',
-  data: {breadcrumbs: ['Employee Management']},
-  component: EmployeeManagementComponent,
-}];
+const routes: Routes = [
+  {
+    path: '',
+    data: { breadcrumbs: ['Employee Management'] },
+    component: EmployeeManagementComponent,
+    children: [
+      {
+        path: '',
+        component: EmployeeListComponent,
+      },
+      {
+        path: 'detail',
+        component: EmployeeDetailComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class EmployeeManagementRoutingModule { }
+export class EmployeeManagementRoutingModule {}
--- src/app/components/employee-management/employee-management.module.ts
+++ src/app/components/employee-management/employee-management.module.ts
@@ -2,6 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { AvatarModule } from 'primeng/avatar';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { MenuModule } from 'primeng/menu';
+
 import { AppLayoutModule } from 'src/app/layout/app-layout.module';
 import { AppTopbarModule } from 'src/app/layout/app-topbar.module';
 import { EmployeeListComponent } from './components/employee-list/employee-list.component';
@@ -12,11 +18,13 @@ import { EmployeeStore } from './store/employee-management.store.service';
 import { ShareModule } from '../share/share.module';
 import { EmployeeItemComponent } from './components/employee-item/employee-item.component';
 import { EmployeeCarouselModule } from './components/employee-carousel/employee-carousel.module';
+import { EmployeeDetailComponent } from './components/employee-detail/employee-detail.component';
 @NgModule({
   declarations: [
     EmployeeManagementComponent,
     EmployeeListComponent,
     EmployeeItemComponent,
+    EmployeeDetailComponent,
   ],
   imports: [
     CommonModule,
@@ -25,7 +33,12 @@ import { EmployeeCarouselModule } from './components/employee-carousel/employee-
     RouterModule,
     AppTopbarModule,
     ShareModule,
-    EmployeeCarouselModule
+    EmployeeCarouselModule, 
+    AvatarModule, 
+    TableModule,
+    ButtonModule,
+    ToastModule,
+    MenuModule
   ],
   providers: [EmployeeManagementService, EmployeeStore]
 })
